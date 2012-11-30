41ee9fab2229c782ce96fb5c8556ba4cf341c1f0
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2012-11-30 09:40:45 +0000

    add a test with adel
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2282 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ alep/test/test_protocol_adel.py
@@ -0,0 +1,32 @@
+""" Test the protocol of communication between adel and alep """
+
+from alinea.adel.newmtg import *
+from alinea.adel.mtg_interpreter import *
+from openalea.plantgl.all import *
+import alinea.adel.fitting as fitting
+
+
+def leaves_db():
+    import cPickle as Pickle
+    fn = r'../../adel/adel/data/leaves_simple.db'
+    f = open(fn)
+    leaves = Pickle.load(f)
+    f.close()
+    leaves = fitting.fit_leaves(leaves, 9)
+    return leaves
+
+
+def adel_mtg():
+    """ create a very simple adel mtg """
+    d = {'plant':[1,1],'axe':[0,1],'numphy':[1,1], 
+         'Laz': [0,90], 'Ll' :[3,3], 'Lv' :[3,3] , 'Lsen':[0,0], 'L_shape':[3,3], 'Lw_shape':[.3,.3], 'Linc':[0,0],
+         'Einc':[0,45],'El':[1,1],'Ev':[1,1],'Esen':[0,0],'Ed': [0.1,0.1]}
+    g=mtg_factory(d,adel_metamer,leaf_db=leaves_db())
+    g=mtg_interpreter(g)
+    return g
+    
+    
+def test_adel():
+    g = adel_mtg()
+    scene = plot3d(g)
+    Viewer.display(scene)    
