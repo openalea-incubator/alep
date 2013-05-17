947fe0b0c9b003bcb3948977b83e76deddd92fff
Guillaume Garin <guillaume.garin@itk.fr>
2013-05-17 13:17:13 +0000

    Add set_properties in tutorial_powdery_mildew_vine.py
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2732 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/example/tutorial_powdery_mildew_vine.py
+++ alep/example/tutorial_powdery_mildew_vine.py
@@ -6,12 +6,24 @@ from alinea.alep.vine import Vine
 
 from alinea.alep.inoculation import RandomInoculation
 from alinea.alep.growth_control import NoPriorityGrowthControl
+from alinea.alep.dispersal import RandomDispersal
+from alinea.alep.architecture import *
 from alinea.alep.powdery_mildew import *
 from alinea.alep.protocol import *
 
 vine = Vine()
 g0 = vine.setup_canopy(age=6)
 vine.plot(g0)
+
+# Add missing properties needed for the simulation
+# The simulation requires the following properties on leaf elements:
+#   - 'surface': total surface of the leaf element (in cm2)
+#   - 'healthy_surface': surface of the leaf element without lesion or senescence (in cm2)
+#   - 'age': age of the leaf (in hours)
+#   - 'position_senescence': position of the senescence on blade axis
+set_properties(g,label = 'lf',
+               surface=5, healthy_surface=5, 
+               age = 0., position_senescence=None)
 
 fungus = powdery_mildew()
 PowderyMildewDU.fungus=fungus
@@ -22,11 +34,23 @@ initiate(g0,dispersal_units,inoculator, label='lf')
 
 dt=1
 controler = NoPriorityGrowthControl()
+dispersor = RandomDispersal()
 nsteps = 2
 
 g=g0
 
 for i in range(nsteps):
     g = vine.grow(g,dt)
+    set_properties(g,label = 'LeafElement',
+                    wetness=True,
+                    temp=22.,
+                    rain_intensity=0.,
+                    rain_duration=0.,
+                    relative_humidity=85.,
+                    wind_speed=0.2,
+                    age=i+dt)
     infect(g,dt)
     update(g,dt,controler)
+    
+    scene = plot3d(g)
+    disperse(g, scene, dispersor, "PowderyMildew")
