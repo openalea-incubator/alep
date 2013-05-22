62c489dbc639d0027d4c00f752c993941ac0c32b
Guillaume Garin <guillaume.garin@itk.fr>
2013-05-22 07:48:14 +0000

    Bug fixes on visualea nodes
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2750 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/src/alinea/alep_wralea/Test_nodes/test_protocol_nodes.py
+++ alep/src/alinea/alep_wralea/Test_nodes/test_protocol_nodes.py
@@ -60,6 +60,61 @@ def set_properties_node(g, label='LeafElement', dict=None):
     return set_properties(g, label=label, **dict)
     
 # Operations on fungal objects #######################################
-from alinea.alep.disease_operation import (distribute_dispersal_units,
-                                           distribute_lesions)
+from alinea.alep.disease_operation import distribute_disease
+from alinea.alep.inoculation import RandomInoculation
+
+def distribute_dispersal_units(g, nb_objects=1, 
+                               disease_model='powdery_mildew',
+                               initiation_model=RandomInoculation()):
+    """ Use the method 'distribute_disease' to distribute dispersal units on g. 
+    
+    Parameters
+    ----------
+    g: MTG
+        MTG representing the canopy
+    nb_objects: int
+        Number of dispersal units or lesions to distribute on the MTG
+    disease_model: model
+        Type of model to compute disease lesion development
+    initiation_model: model
+        Model that sets the position of each DU/lesion in stock on g
+        Requires a method named 'allocate' (see doc)
+        
+    Returns
+    -------
+    g: MTG
+        Updated MTG with dispersal units
+    """
+    distribute_disease(g, fungal_object='dispersal_unit', 
+                       nb_objects=nb_objects, 
+                       disease_model=disease_model,
+                       initiation_model=initiation_model)
+                               
+def distribute_lesions(g, nb_objects=1, 
+                       disease_model='powdery_mildew',
+                       initiation_model=RandomInoculation()):
+    """ Use the method 'distribute_disease' to distribute lesions on g. 
+    
+    Parameters
+    ----------
+    g: MTG
+        MTG representing the canopy
+    nb_objects: int
+        Number of dispersal units or lesions to distribute on the MTG
+    disease_model: model
+        Type of model to compute disease lesion development
+    initiation_model: model
+        Model that sets the position of each DU/lesion in stock on g
+        Requires a method named 'allocate' (see doc)
+        
+    Returns
+    -------
+    g: MTG
+        Updated MTG with lesions
+    """
+    distribute_disease(g, fungal_object='lesion', 
+                       nb_objects=nb_objects, 
+                       disease_model=disease_model,
+                       initiation_model=initiation_model)                            
+
     
