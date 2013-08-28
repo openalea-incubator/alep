8bde4cd48887af425c25d5ff94bfe126ce6907f5
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2013-08-28 13:31:42 +0000

    Heights are weighted by the surface of shapes in list for each leaf
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@3046 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/example/draft_wind_distribution.py
+++ alep/example/draft_wind_distribution.py
@@ -3,11 +3,13 @@
 from alinea.alep.vine import Vine
 from alinea.adel.mtg_interpreter import plot3d
 from openalea.plantgl.all import Viewer
-from alinea.astk.plantgl_utils import get_height
+from alinea.astk.plantgl_utils import *
 from alinea.weather.mini_models import wind_speed_on_leaf
 from alinea.alep.architecture import get_leaves, set_property_on_each_id
 from alinea.alep.alep_color import alep_colormap, green_yellow_red
     
+import numpy as np
+
 # Color leaves according to wind speed at their height #############
 def update_plot(g, global_wind_speed=5.):
     g = alep_colormap(g, 'wind_speed', cmap='jet', 
@@ -33,9 +35,12 @@ global_wind_speed = 5.
 
 # Estimate the distribution of wind speed within the canopy
 leaves = get_leaves(g, label='lf')
-geometries = {k:geom for k, geom_list in g.property('geometry').iteritems() for 
-                geom in geom_list if k in leaves}
-heights = get_height(geometries)
+# geometries = {k:geom for k, geom_list in g.property('geometry').iteritems() for 
+                # geom in geom_list if k in leaves}
+geometries = {k:geom for k, geom in g.property('geometry').iteritems() if k in leaves}
+h = get_height(geometries)
+surf,_=get_area_and_normal(geometries)
+heights = dict([(k,np.average(h[k],weights= surf[k])) for k in h])
 wind_speeds = {}
 for leaf in leaves:
     wind_speeds.update({leaf:wind_speed_on_leaf(wind_speed=global_wind_speed, 
