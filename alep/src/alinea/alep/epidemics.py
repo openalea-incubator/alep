5b54b5b7ab033723a99dd577d30896b2993f99f1
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2013-11-27 10:47:22 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@3376 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/src/alinea/alep/epidemics.py
+++ alep/src/alinea/alep/epidemics.py
@@ -8,11 +8,11 @@ class SeptoriaSepto3D(object):
     from alinea.alep.infection_control import BiotrophDUPositionModel
     from alinea.alep.senescence import WheatSeptoriaPositionedSenescence
     from alinea.alep.dispersal_emission import SeptoriaRainEmission
-    from alinea.septo3d.alep_interfaces import Septo3DSplash
+    from alinea.septo3d.dispersion.alep_interfaces import Septo3DTransport
     from alinea.alep.washing import RapillyWashing
 
    
-    def __init__(g,label='LeafElement', domain_area=None):
+    def __init__(g,label='LeafElement', domain, domain_area, convUnit):
     #a priori si on considere ces attributs comme generique, plutot viser un ininit de ces champs sans argument, et faire le setting ensuite. Ceci afin d'etre compatible avec plugin (qui ne remplace que l'etape import)
         self.disease = plugin_septoria()
         self.inoculator = RandomInoculation()
@@ -20,6 +20,6 @@ class SeptoriaSepto3D(object):
         self.infection_controler = BiotrophDUPositionModel()
         self.sen_model = WheatSeptoriaPositionedSenescence(g, label='LeafElement')
         self.emitter = SeptoriaRainEmission()
-        self.transporter = Septo3DSplash(reference_surface=domain_area)
+        self.transporter = Septo3DTransport(domain=domain, domain_area=domain_area, convUnit = convUnit)
         self.washor = RapillyWashing()
     
