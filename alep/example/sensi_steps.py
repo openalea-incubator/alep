33983c6829746b7766cb43f8b661ce9d71faab63
Guillaume Garin <guillaume.garin@itk.fr>
2014-07-24 09:28:09 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4425 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/example/sensi_steps.py
+++ alep/example/sensi_steps.py
@@ -2,14 +2,18 @@ import multiprocessing
 import numpy as np
 from poc_sensi import *
 
-def sensi_step(num_step):
-    print '\nstep %d' % num_step
-    param_values = np.loadtxt('SGInput'+str(num_step)+'.txt', delimiter=' ')
-    Y = evaluate(param_values)
+param_values = np.loadtxt('SGInput.txt', delimiter=' ')
+
+def sensi_step(num_step, nb_cpu):
+    nb_params = int(len(param_values)/nb_cpu)
+    Y = evaluate(param_values[nb_params*(num_step-1):nb_params*num_step])
     np.savetxt('SGOutput'+str(num_step)+'.txt', Y, delimiter=' ')
     return
     
 if __name__ == '__main__':
-    for i in range(8):
-        p = multiprocessing.Process(target=sensi_step, args=(i+1,))
+    import pdb
+    pdb.set_trace()
+    nb_cpu = multiprocessing.cpu_count()
+    for i in range(nb_cpu):
+        p = multiprocessing.Process(target=sensi_step, args=(i+1, nb_cpu))
         p.start()
