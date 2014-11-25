7705ccdbef1c88f09e6e095d4378d1c7b6be3d3a
Guillaume Garin <guillaume.garin@itk.fr>
2014-11-25 14:56:53 +0000

    changes on multi_make_canopy
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4871 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/example/multi_make_canopy.py
+++ alep/example/multi_make_canopy.py
@@ -9,9 +9,10 @@ except:
     import pickle
         
 def make_canopies((yr, variety, nplants, nsect, wheat_path)):
-    make_canopy(start_date = str(int(yr))+"-10-15 12:00:00", end_date = str(int(yr+1))+"-08-01 00:00:00",
+    make_canopy(start_date = str(int(yr-1))+"-10-15 12:00:00", end_date = str(int(yr))+"-08-01 00:00:00",
             variety = variety, nplants = nplants, nsect = nsect, disc_level = 5, dir = wheat_path)
         
 def multi_make_canopies(combinations):
     nb_cpu = cpu_count()
-    pymap(make_canopies, combinations, nb_cpu)
+    # pymap(make_canopies, combinations, min(len(combinations), nb_cpu))
+    pymap(make_canopies, combinations, 1)
