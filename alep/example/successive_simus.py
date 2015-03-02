d75bfbd25349fc78c7a2e45f64acdb712234ccc0
Guillaume Garin <guillaume.garin@itk.fr>
2015-03-02 16:21:42 +0000

    Temporary example for successive simus
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@5062 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ alep/example/successive_simus.py
@@ -0,0 +1,19 @@
+from septo_decomposed import *
+
+def simu(nb_plants):
+    dir_path = './adel/tremie_2012_'+str(nb_plants)+'pl_7sect'
+    g, recorder = run_disease(start_date="2011-10-21 12:00:00", end_date="2012-07-18 00:00:00", 
+                                variety = 'Tremie12', nplants=nb_plants, nsect=7, 
+                                dir = dir_path, sporulating_fraction = 2e-4,
+                                temp_min = 0., degree_days_to_chlorosis = 150, 
+                                age_physio_switch_senescence = 0.01, 
+                                distri_chlorosis = {'mu':150., 'sigma':30.})
+    stored_rec = './tremie/recorder_2012_'+str(nb_plants)+'30pl_7sect_frac2e-4_var.pckl'
+    f_rec = open(stored_rec, 'w')
+    pickle.dump(recorder, f_rec)
+    f_rec.close()
+    del recorder
+    del g
+    
+for nb_pl in [1, 5, 10, 20, 50, 100]:
+    simu(nb_pl)
