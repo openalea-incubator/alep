866ebc7cc9fc8c45bda13a751dc5b416a6b3e30c
Guillaume Garin <guillaume.garin@itk.fr>
2015-08-28 16:37:19 +0000

    generate_set_sensi_septo.py
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@5568 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ alep/example/sensitivity/generate_set_sensi_septo.py
@@ -0,0 +1,40 @@
+""" Run sensitivity analysis of Morris of septoria model.
+"""
+
+from collections import OrderedDict
+from disease_sensi_morris import generate_parameter_set
+from sensi_septo_tools import variety_code
+
+parameters = OrderedDict([('sporulating_fraction', [0., 1.e-2]),
+                          ('degree_days_to_chlorosis', [100., 250.]),
+                          ('Smin', [0.001, 0.09]),
+                          ('Smax', [0.1, 1.0]),
+                          ('growth_rate', [0.0001, 0.001]),
+                          ('age_physio_switch_senescence', [0., 1.]),
+                          ('sporulating_capacity', [0., 1.]),
+                          ('density_dus_emitted', [1e3, 5e3]),
+                          ('reduction_by_rain', [0., 1.]), 
+                          ('proba_inf', [0., 1.]), 
+                          ('loss_delay', [72., 168.]), 
+                          ('temp_min', [0., 10.])])
+                          
+v = variety_code()
+
+# scenarios = [(2011, v['Mercia']), 
+             # (2011, v['Rht3']),
+             # (2012, v['Tremie12']),
+             # (2013, v['Tremie13'])]
+scenarios = [(2013, v['Tremie13'])]
+
+list_param_names = ['i_sample', 'i_boot', 'year', 'variety'] + parameters.keys()
+nboots = 5
+generate_parameter_set(parameters,
+                       scenarios,
+                       parameter_range_file = './septoria/septo_param_range.txt',
+                       sample_file = './septoria/septo_morris_input.txt',
+                       num_trajectories = 10,
+                       num_levels = 5,
+                       grid_jump = 1,
+                       optimal_trajectories = None,
+                       nboots = nboots)
+    
