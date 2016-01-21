c4c60cfe3c1a043c39be8bb712e641d7d4b0b38b
Guillaume Garin <guillaume.garin@itk.fr>
2016-01-21 07:26:22 +0000

    Debug in get_synthetic_outputs_by_leaf
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@5823 0978ab36-7443-0410-8248-c2aa344a765a

--- alep/src/alinea/alep/disease_outputs.py
+++ alep/src/alinea/alep/disease_outputs.py
@@ -1142,7 +1142,10 @@ def get_synthetic_outputs_by_leaf(data, return_conf=False):
         df_lf = data[data['num_leaf_top']==lf]
         audpcs = numpy.unique(df_lf['audpc'])
 #        audpcs_500 = numpy.unique(df_lf['audpc_500'])
-        max_sevs = df_lf.groupby(['rep', 'num_plant']).max()['severity']
+        if 'rep' in df_lf.columns:
+            max_sevs = df_lf.groupby(['rep', 'num_plant']).max()['severity']
+        else:
+            max_sevs = df_lf.groupby(['num_plant']).max()['severity']
         max_sevs = max_sevs.reset_index()
         max_sevs = max_sevs.groupby('rep').mean()['severity']
         audpcs = df_lf.groupby(['rep', 'num_plant']).max()['audpc']
