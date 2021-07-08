08614bf2ca845cb324be13537e72b33419f6e046
Marc LABADIE <labadiemarc89@gmail.com>
2021-07-08 14:56:49 +0200

    WIP software paper tutorial from echap

--- example/software_paper_tutorial.py
+++ example/software_paper_tutorial.py
@@ -2,22 +2,58 @@
 
 # 1. Get echap reconstruction
 
-from alinea.echap.architectural_reconstructions import EchapReconstructions
+from pathlib import Path
 
-echap = EchapReconstructions()
+from alinea.echap.architectural_reconstructions import EchapReconstructions, echap_reconstructions
+
+from alinea.alep.architecture import update_healthy_area
+
+from alinea.adel.newmtg import move_properties
+
+
+filename= 'echap_reconstruction.pckl'
+
+#if not os.path.exists(filename):
+if not Path(filename).exists():
+    echap = EchapReconstructions()
+    echap.save(filename=filename)
+else:
+    echap= EchapReconstructions.load(filename=filename)
+
 adel = echap.get_reconstruction(name="Mercia", nplants=2,seed=0)
 
 # 2. run simulation 
-timestep = 30 #Day degree
-steps = 3
 
+#init sumulation
+timestep = 30 #Day degree
+steps = 10
+
+
+
+# 2.1 Grow wheat canopy and vizualized development
+
+## init canopy
 canopy_age=300
 g = adel.setup_canopy(age=canopy_age)
 
+## init alep
+### Add the property 'healthy_area' on the leaves
+update_healthy_area(g, label = 'LeafElement')
+
 adel.plot(g)
-
-
 for i in range(steps):
     canopy_age+=timestep
-    g = adel.setup_canopy(age=canopy_age)
+    
+    # update canopy
+    newg = adel.setup_canopy(age=canopy_age)
+    adel.canopy_age=canopy_age
+    move_properties(g, newg)
+    g= newg
+    update_healthy_area(g, label = 'LeafElement')
     adel.plot(g)
+    
+# 3. Disease development (septo3D)
+    
+## Initialize the models for septoria
+septoria = plugin_septoria()
+
